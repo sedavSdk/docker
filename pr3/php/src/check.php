9f4fceacda18800820753e47895e418329196ec9
sedavSdk <sedavj1@gmail.com>
2022-09-29 20:27:21 +0300

    29

--- pr3/php/src/check.php
+++ pr3/php/src/check.php
@@ -1,7 +1,7 @@
 <?php
 $password = $_GET['psw'];
 $login= $_GET['uname'];
-if($login == 'sedav' and $password = '1321'){
+if($login == 'sedav' and $password == '1321'){
     header("Location: http://localhost:8000/index.php");
     exit();
 } else {
